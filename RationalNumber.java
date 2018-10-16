ad62abbd94657932d6785dee7d5c4128af0aca3f
rleongPd5 <rleong00@stuy.edu>
2018-10-16 04:22:49 -0400

    Added instance variables

---
+++ RationalNumber.java
@@ -0,0 +1,10 @@
+public class RationalNumber{
+  //Instance variables
+  private int numerator;
+  private int denominator;
+  //Constructor to make a rational RealNumber given integers
+  public RationalNumber(int num, int denom){
+
+  }
+
+}
---
+++ RealNumber.java
@@ -0,0 +1,16 @@
+public class RealNumber{
+  //Instance variables
+  private double val;
+  //Constructor for making a real number
+  public RealNumber(double num){
+
+  }
+  //Get variables
+  public double getValue(){
+    return this.val;
+  }
+  //toString
+  public String toString(){
+
+  }
+}
