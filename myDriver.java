5c300bf4dbc44d3f6f76eeb479dd59626cd3ce4c
rleongPd5 <rleong00@stuy.edu>
2018-10-16 21:02:47 -0400

    Added testcases for operations in RealNumber

--- myDriver.java
+++ myDriver.java
@@ -1,6 +1,12 @@
 public class myDriver{
   public static void main(String[] args){
     RealNumber a = new RealNumber(2.5);
+    RealNumber b = new RealNumber(7.5);
     System.out.println(a);
+    System.out.println(b);
+    System.out.println(a.add(b)); //Should print 10.0
+    System.out.println(a.multiply(b));//Should print 18.75
+    System.out.println(b.divide(a));//Should print 3.0
+    System.out.println(b.subtract(a));//Should print 5.0
   }
 }
