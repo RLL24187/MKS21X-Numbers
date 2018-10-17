45f3d1a9f5e46da65f5292775773cbe93a85b2a8
rleongPd5 <rleong00@stuy.edu>
2018-10-17 00:03:35 -0400

    Added more test cases, but all are now commented out

--- myDriver.java
+++ myDriver.java
@@ -10,25 +10,37 @@ public class myDriver{
         System.out.println(a);
         System.out.println(b);
         */
-      //Testing operations
+      //System.out.println("Testing operations:");
         /*
+        System.out.println("2.5 + 7.5 = 10.0");
         System.out.println(a.add(b)); //Should print 10.0
+        System.out.println("2.5 * 7.5 = 18.75");
         System.out.println(a.multiply(b));//Should print 18.75
+        System.out.println("7.5 / 2.5 = 3.0");
         System.out.println(b.divide(a));//Should print 3.0
+        System.out.println("Should print 0.0");
         System.out.println(zero.divide(b));//Should print 0.0
+        System.out.println("Should print Error Message");
         System.out.println(a.divide(zero));//Should print Error
+        System.out.println("Should print 5.0");
         System.out.println(b.subtract(a));//Should print 5.0
         */
-      //Testing equal functions
+      //System.out.println("\n\nTesting equal functions:");
         /*
+        System.out.println("Should print false");
         System.out.println(a.equals(b));//Should print false
+        System.out.println("Should print false");
         System.out.println(a.equals(a));//Should print true
+        System.out.println("Should print true");
         System.out.println(a.equals(c));//Should print true
         */
-      //Testing greaterThan function;
+      //System.out.println("\n\nTesting greaterThan function:");
         /*
+        System.out.println("Should print false");
         System.out.println(a.greaterThan(b));//Should print false
+        System.out.println("Should print false");
         System.out.println(a.greaterThan(a));//Should print false
+        System.out.println("Should print true");
         System.out.println(b.greaterThan(c));//Should print true
         */
     RationalNumber A = new RationalNumber(1, 2);
@@ -37,20 +49,27 @@ public class myDriver{
     RationalNumber D = new RationalNumber(-9, 6);
     RationalNumber error = new RationalNumber(8, 0);
     RationalNumber Zero = new RationalNumber(0, -16);
-    //Testing RationalNumber.java
-      //Testing toString
+    //System.out.println("\n\nTesting RationalNumber.java:");
+      //System.out.println("Testing toString");
         /*
+        System.out.println("Should print 1/2");
         System.out.println(A);
+        System.out.println("Should print 0/3");
         System.out.println(B);
+        System.out.println("Should print 2/4");
         System.out.println(C);
+        System.out.println("Should print -9/6");
         System.out.println(D);
         */
-      //Testing getValue
-      System.out.println(A);
-      System.out.println(B);
-      System.out.println(C);
-      System.out.println(D);
-      System.out.println(Zero);
+      //System.out.println("Testing getValue");
+        /*
+        System.out.println("Should print 0.5");
+        System.out.println(A);
+        System.out.println("Should print 0.0");
+        System.out.println(B);
+        System.out.println("Should print 0.0");
+        System.out.println(Zero);
+        */
       //Testing operations
         //Testing Reciprocal
           /*
@@ -73,9 +92,11 @@ public class myDriver{
         System.out.println(D); //Should get -3/2
         */
       //Testing Operations
+        /*
         System.out.println(A.add(C)); //Should get 1
         System.out.println(A.subtract(C)); //Should get 0
         System.out.println(Zero.multiply(B)); //Should get 0
         System.out.println(D.divide(A)); //Should get -3
+        */
   }
 }
