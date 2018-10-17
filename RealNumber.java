d979f6c3f42bbe1dc32d7b751c100c0438a6fe34
rleongPd5 <rleong00@stuy.edu>
2018-10-16 20:56:35 -0400

    Fixed functions, added toString testcase

--- RealNumber.java
+++ RealNumber.java
@@ -1,40 +1,44 @@
 public class RealNumber{
   //Instance variables
-  private double val;
+  private double value;
   //Constructor for making a real number
   public RealNumber(double num){
-
+    value = num;
   }
   //Get variables
   public double getValue(){
-    return this.val;
+    return value;
   }
   //toString
   public String toString(){
-    return "";
+    return "" + value;
   }
   //Setters and basic operations
-  public double add(double num){
-    return 1.;
+  /*Return the sum of this and the other*/
+  public double add(RealNumber other){
+    return value + other.getValue();
   }
-  public double subtract(double num){
-  return 1.;
+  /*Return the product of this and the other*/
+  public double multiply(RealNumber other){
+    return value * other.getValue();
   }
-  public double multiply(double num){
-    return 1.;
+  /*Return the this divided by the other*/
+  public double divide(RealNumber other){
+    return value / other.getValue();
   }
-  public double divide(double num){
-    return 1.;
+  /*Return the this minus the other*/
+  public double subtract(RealNumber other){
+    return value - other.getValue();
   }
   //Comparison functions
-  public boolean equals(double num){
-    return true;
+  public boolean equals(RealNumber num){
+    return value == num.getValue();
   }
-  public boolean greaterThan(double num){
-    return false;
+  public boolean greaterThan(RealNumber num){
+    return value > num.getValue();
   }
   //Method that returns a copy of RealNumber
   public RealNumber copy(){
-    return new RealNumber(this.val);
+    return new RealNumber(this.value);
   }
 }
--- myDriver.java
+++ myDriver.java
@@ -1,5 +1,6 @@
 public class myDriver{
   public static void main(String[] args){
-    
+    RealNumber a = new RealNumber(2.5);
+    System.out.println(a);
   }
 }
